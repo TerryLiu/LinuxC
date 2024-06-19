b821941b1c68dcca751e3e80a1d6da4e09b92e7e
lanjinger/liuzhong <liuzhong@cls.cn>
2024-06-19 17:36:49 +0800

    +注释

--- APUE/IPC/usepipe.c
+++ APUE/IPC/usepipe.c
@@ -10,37 +10,38 @@
 
 int main()
 {
-    int pd[2];
-    pid_t pid;
+    int pd[2]; // 声明一个fd的数组空间
+    pid_t pid; // 声明一个子进程的pid
     char buf[BUFSIZE];
-
+    // 检查创建匿名管道是否成功
     if (pipe(pd) < 0){
         perror("pipe()");
         exit(1);
     }
 
     fprintf(stdout,"hello,welcome to MusicPlayer\n");
-
+    // 创建一个子进程
     pid = fork();
     if (pid < 0){
         perror("fork()");
         exit(1);
     }
-    
+    // 如果是子进程,就读管道数据
     if(pid == 0){
         close(pd[1]); //关闭写端
 
-        dup2(pd[0],0);//将将读端重定向到stdin mpg123 - "-" 表示从stdin读取
+        dup2(pd[0],0);//将读端重定向到 标准输入,即用pd[0]来填充到0号文件描述符
         close(pd[0]);
 
         int fd = open("/dev/null",O_RDWR);//屏蔽其他标准输出
-        dup2(fd,1);
-        dup2(fd,2);
-
+        dup2(fd,1); //用一个无效文件描述符来填充 标准输出
+        dup2(fd,2); //用一个无效文件描述符来填充 标准错误
+        // mpg123 - 表示从stdin读取
         execl("/bin/mpg123","mpg123","-",NULL);
         perror("execvp()");
         exit(1);
     }else{
+        // 如果是父进程,就写管道数据
         close(pd[0]);//关闭读端
         int fd = open("./test.mp3",O_RDONLY);
         int len;
@@ -56,7 +57,7 @@ int main()
             if (len == 0){
                 break;
             }
-            write(pd[1],buf,len);
+            write(pd[1],buf,len); // 向管道写端写入数据
         }
         close(pd[1]);
         wait(NULL);
